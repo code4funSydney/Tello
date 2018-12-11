615e9d64a856a26a72d43b750ff9d58a6c636396
Daniel Byron <danielbyron51@gmail.com>
2018-12-11 15:57:26 +1100

    Updated setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 setup(
       name = 'Tello',
       version = '0.1',
-      packages = find_packages(),
+      py_modules=['tello'],
       install_requires = [
           'opencv-python'
       ]
